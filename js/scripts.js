1dc3b9ddb525ce38ef87663edfebb01f9be1842b
byeol <dksalsquf699@naver.com>
2023-06-19 15:00:04 +0900

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,129 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// skills bar
+var skills = {
+    ht: 85,
+    jq: 75,
+    sk: 65,
+    ph: 50,
+    il: 55,
+    in: 90,
+    fl: 95
+  };
+  
+  $.each(skills, function(key, value) {
+    var skillbar = $("." + key);
+  
+    skillbar.animate(
+      {
+        width: value + "%"
+      },
+      3000,
+      function() {
+        $(".speech-bubble").fadeIn();
+      }
+    );
+  });
+  
+
+// 이미지 슬라이드
+let imgIndex = 0;
+let position = 0;
+const IMG_WIDTH = 438;
+const $btnPrev = document.querySelector(".btn-prev");
+const $btnNext = document.querySelector(".btn-next");
+const $slideImgs = document.querySelector(".slide-images");
+
+let prev = function () {
+  if (imgIndex > 0) {
+    $btnNext.removeAttribute("disabled");
+    position += IMG_WIDTH;
+    $slideImgs.style.transform = `translateX(${position}px)`;
+    imgIndex = imgIndex - 1;
+  }
+  if (imgIndex == 0) {
+    $btnPrev.setAttribute("disabled", "true");
+  }
+};
+
+let next = function () {
+  if (imgIndex < 5) {
+    $btnPrev.removeAttribute("disabled");
+    position -= IMG_WIDTH;
+    $slideImgs.style.transform = `translateX(${position}px)`;
+    $slideImgs.style.transition = "transform .5s ease-out";
+    imgIndex = imgIndex + 1;
+  }
+  if (imgIndex == 4) {
+    $btnNext.setAttribute("disabled", "true");
+  }
+};
+
+let init = function () {
+  $btnPrev.setAttribute("disabled", "true");
+  $btnPrev.addEventListener("click", prev);
+  $btnNext.addEventListener("click", next);
+};
+
+init();
+
+// resume
+'use strict';
+
+$(document).on('click', '.accordion-list .accordion-item > a', function(e){
+  e.preventDefault();
+  var $this = $(this);
+  var target = $this.parent();
+  var description = $this.siblings('.accordion-desc');
+  var other = target.siblings('.accordion-item');
+  var otherDescription = other.find('.accordion-desc');
+  accordionToggle(target, description, other, otherDescription);
+});
+
+function accordionToggle(target, description, other, otherDescription){
+  if (target.hasClass('active')) {
+    target.removeClass('active');
+    description.stop().slideUp(300);
+  } else {
+    target.addClass('active');
+    description.stop().slideDown(300);
+  }
+
+  if (other && otherDescription) {
+    other.removeClass('active');
+    otherDescription.stop().slideUp(300);
+  }
+};
